71bf05bad48fa4b0a7e585262457a1aa2c7c0c34
Jannis <jbauer94@icloud.com>
2024-11-10 20:38:14 +0100

    Initial Commit

---
+++ BipTheGuy/BipTheGuyApp.swift
@@ -0,0 +1,17 @@
+//
+//  BipTheGuyApp.swift
+//  BipTheGuy
+//
+//  Created by Jannis Bauer on 10.11.24.
+//
+
+import SwiftUI
+
+@main
+struct BipTheGuyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
